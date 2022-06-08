b452c21928dfe6fd2b1783e463950e8b06e55d63
Reese Eifler <reeseeifler@gmail.com>
2022-06-08 11:55:21 -0400

    EditEnd fixed, everything updates properly

--- price-board/src/app/reducers/commodities-list.reducer.ts
+++ price-board/src/app/reducers/commodities-list.reducer.ts
@@ -4,11 +4,9 @@ import { Commodity } from 'src/app/models/commodity.model'
 import * as CommodityActions from '../actions/commodity.actions'
 import { Populate, EditStart, EditEnd, Randomize } from '../actions/commodity.actions'
 
-
 const getVals = () => {
     const start = parseFloat((Math.random() * 100).toFixed(2))
     const end = parseFloat((Math.random() * 100).toFixed(2))
-    //const trend = start > end ? true : false
     return {
         start, end
     }
@@ -17,7 +15,6 @@ const getVals = () => {
 const initialState: any = {
     rowData: []
 }
-
 
 export const commoditiesListReducer = createReducer(
     initialState,
@@ -35,13 +32,6 @@ export const commoditiesListReducer = createReducer(
     ),
     on(EditStart, 
         (state, { id, start_price }) => {
-            // console.log('new',id,start_price)
-            // console.log('row',{
-            //     id,
-            //     start_price,
-            //     end_price: state[id].end_price,
-            //     trend: start_price > state[id].end_price ? false : true
-            // })
             let newRows = state.rowData.map((el:any) => {
                 if(el.id === id) {
                     const end_price = state.rowData[id].end_price
@@ -59,34 +49,31 @@ export const commoditiesListReducer = createReducer(
                 ...state,
                 rowData: newRows
             }
-            //const end_price = state[id].end_price
-            console.log(newState)
-            //newState.rowData[id] = {}
-
             return newState
         }
     ),
     on(EditEnd, 
         (state, { id, end_price }) => {
-            console.log('new end_price',end_price)
-            console.log('row',{
-                id,
-                start_price: state[id].start_price,
-                end_price,
-                trend: state[id].end_price > end_price ? false : true
-            })
-            let newState = state
-            const start_price = state[id].start_price
-            return {
-                ...state,
-                [id]: {
-                    id,
-                    start_price,
-                    end_price,
-                    trend: start_price > end_price ? false : true
+            let newRows = state.rowData.map((el:any) => {
+                if(el.id === id) {
+                    const start_price = state.rowData[id].start_price
+                    return {
+                        id,
+                        start_price,
+                        end_price,
+                        trend: start_price > end_price ?
+                         false : true
+                    }
                 }
+                return el
+            })
+            let newState = {
+                ...state,
+                rowData: newRows
             }
-        }),
+            return newState
+        }
+    ),
     on(Randomize, (state, {id, start_price, end_price}) => ({
         ...state
     }))
